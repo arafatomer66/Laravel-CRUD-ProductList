78ca19dc52e47f1ea23f151db0b621ac6ba6a9f8
Md Omer Arafat <arafatomer66@gmail.com>
2018-08-06 01:38:53 -0700

    Initial commit

---
+++ resources/views/products/layout.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Product List CRUD</title>
+	<link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-alpha/css/bootstrap.css" rel="stylesheet">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+</head>
+<body>
+
+
+<div class="container">
+    @yield('content')
+</div>
+
+
+</body>
+</html>
